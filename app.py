b0e10404de28464694b430ee11eb67d03cb88ac9
Michał Midor <contact@midor.me>
2025-10-30 13:58:02 +0100

    Fix docker caching

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello, World! This is my app in a Docker container."
+    return "Hello, World! This is my app in a Docker container, build with Ansible."
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+    
+    
